3583fb3caa7c070f58aeacad454f06daf009f4c0
xiezihao011004 <1483767394@qq.com>
2019-11-05 21:16:22 +0800

    xiezihao011004

--- lab2/ex2.c
+++ lab2/ex2.c
@@ -2,53 +2,22 @@
 int bitcount(unsigned x);
 int main(void)
 {
-   int a, b, c, d, e;
-   a = getchar(); 
-   b = getchar();
-   if(b != '\n')
-     {
-       c = getchar();
-        if(c != '\n')
-          {
-            d = getchar();
-            if(d != '\n')
-            {
-              e = getchar();
-              if(e !='\n')
-              {
-                getchar();//读取回车
-              }
-            }
-          }
-     }      
-   int n;
-   if (b == '\n')
-     {   
-       n = a -'0';
-       printf("输入数字二进制表达中二进制位是1的个数为%d\n",bitcount(n));
-       }
-   else if(c == '\n')
-     {
-       n = (a - '0')* 10 + b - '0';
-       printf("输入数字二进制表达中二进制位是1的个数为%d\n",bitcount(n));
-     }
-  else if(d == '\n')
-     {
-       n = (a - '0') * 100 + (b -'0')* 10 + c - '0';
-       printf("输入数字二进制表达中二进制位是1的个数为%d\n",bitcount(n));
-     }
-  else if(e == '\n')
+    char digit[6];
+    int i = 0;
+    int n = 0;
+    while (i < 7)
     {
-      n = (a - '0') * 1000 +(b - '0') * 100 + (c - '0') * 10 + d - '0';
-      printf("输入数字二进制表达中二进制位是1的个数为%d\n",bitcount(n));
+            digit[i] = getchar();
+            if(digit[i] == '\n')
+            {
+                break;
+            }
+            n = n * 10 + (digit[i] - '0');
+            i++ ; 
     }
-  else
-     {
-       n = (a - '0') * 10000 + (b - '0') * 1000 +(c - '0') * 100 + (d - '0') * 10 + e  - '0';
-       printf("输入数字二进制表达中二进制位是1的个数为%d\n",bitcount(n));
-     }
-     printf("你当时输入的数字是%d\n",n);
-       return 0;
+    printf("输入数字%d\n", n);
+    printf("二进制中1的个数%d\n", bitcount(n));    
+    return 0;
 }
 int bitcount(unsigned x)
 {
