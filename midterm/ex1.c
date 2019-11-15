64f8950eeb505bc4872942275547076eca4b3e6d
xiezihao011004 <56293384+xiezihao011004@users.noreply.github.com>
2019-11-15 17:16:28 +0800

    Create ex1.c

---
+++ midterm/ex1.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+int main()
+{
+    int i = 0;
+    printf("Enter any characters not more than 50 characters:");
+    int digit[i];
+    int j = 0;
+    int k = 0;
+    while (i <= 50)
+    {
+        digit[i] = getchar();
+        if (digit[i] == '\n')
+        {
+            break;
+        }
+        else
+        {
+            if ((digit[i] != 'a') && (digit[i] != 'A') && (digit[i] != '0'))
+            {
+                ;
+            }
+            else
+            {
+                if (digit[i] == 'a' || digit[i] == 'A')
+                {
+                    j++;
+                }
+                else if (digit[i] == '0')
+                {
+                    k++;
+                }
+            }
+        }
+        i++;
+    }
+    printf("The number of a is: %d and the number of 0 is: %d\n", j, k);
+    return 0;
+}
